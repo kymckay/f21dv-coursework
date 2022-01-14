1e24357880e909d4f97cd1f8e80f203f487a8384
Kyle Mckay <kymckay.dev@gmail.com>
2022-01-14 17:59:47 +0000

    Add task 11 for lab 1

--- lab1.js
+++ lab1.js
@@ -168,3 +168,25 @@ d3.select('.task8')
       .join('p')
         .text(d => `Patients with heart failure, aged ${d.name}: ${d.count}`);
 })();
+
+// TASK 11
+taskData = [
+    {x: [100,300], y: [100,100], color: 'red'}, // top
+    {x: [300,300], y: [100,300],  color: 'blue'} , // right
+    {x: [300,100], y: [300,300], color: 'yellow'}, // bottom
+    {x: [100,100], y: [300,100], color: 'orange'}, // left
+];
+
+d3.select('.task11')
+  .append('svg')
+    .attr('width', 400)
+    .attr('height', 400)
+    .style('border', '1px solid green')
+  .selectAll('line')
+    .data(taskData)
+  .join('line')
+    .attr('x1', d => d.x[0])
+    .attr('x2', d => d.x[1])
+    .attr('y1', d => d.y[0])
+    .attr('y2', d => d.y[1])
+    .attr('stroke', d => d.color);
