eeba90ef9ae76e07dbf91832a6e15189b81472f0
Kyle Mckay <kymckay.dev@gmail.com>
2022-02-08 21:35:16 +0000

    Add exercise 4 for lab 2

--- lab2/tasks/task4.js
+++ lab2/tasks/task4.js
@@ -1,3 +1,23 @@
-function task4(selector) {}
+function task4(selector) {
+  d3.select(selector)
+    .append('svg')
+      .attr('width', '200px')
+      .attr('height', '200px')
+    .append('circle')
+      .attr('cx', 100)
+      .attr('cy', 100)
+      .attr('r', 50)
+      .attr('fill', 'red')
+      .on('mouseenter', function() {
+        d3.select(this).attr('r', 100);
+      })
+      .on('mouseleave', function() {
+        d3.select(this).attr('r', 50);
+      });
+
+  d3.select(selector)
+  .append('p')
+    .text('The circle will change size when the mouse enters and exits.')
+}
 
 export { task4 }
