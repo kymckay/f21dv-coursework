6015aa7365dd3f1b8a34b5648e38ee67abc67252
Kyle Mckay <kymckay.dev@gmail.com>
2022-02-08 21:48:37 +0000

    Add exercise 5 for lab 2

--- lab2/tasks/task5.js
+++ lab2/tasks/task5.js
@@ -1,3 +1,24 @@
-function task5(selector) {}
+function task5(selector) {
+  const svg = d3.select(selector)
+    .append('svg')
+      .attr('width', '200px')
+      .attr('height', '200px')
+      .style('border', '5px solid black');
+
+  const text = svg.append('text');
+
+  svg.on('pointermove', function(event) {
+    const [x, y] = d3.pointer(event);
+
+    text.attr('x', x + 5)
+        .attr('y', y)
+        .text(`x: ${x}, y: ${y}`);
+  })
+  .on('pointerleave', () => text.text(''));
+
+  d3.select(selector)
+    .append('p')
+      .text('On hover, a text element will follow the cursor.')
+}
 
 export { task5 }
