1e3adbe51eed2a053afbb33b7e4ad2b42de2276d
Kyle Mckay <kymckay.dev@gmail.com>
2022-02-08 21:31:04 +0000

    Add exercise 3 for lab 2

--- lab2/tasks/task3.js
+++ lab2/tasks/task3.js
@@ -1,3 +1,24 @@
-function task3(selector) {}
+function task3(selector) {
+  d3.select(selector)
+    .append('div')
+      .style('width', '100px')
+      .style('height', '100px')
+      .style('background-color', 'green')
+      .on('mouseenter', function() {
+        d3.select(this)
+          .style('background-color', 'orange')
+          .style('border', '5px dashed black');
+      })
+      .on('mouseleave', function() {
+        d3.select(this)
+          .style('background-color', 'steelblue')
+          .style('border', '');
+      });
+
+  d3.select(selector)
+    .append('p')
+      .text('The element will change apperance when the mouse enters and exits.')
+}
+
 
 export { task3 }
