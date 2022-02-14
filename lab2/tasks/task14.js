c2ae6fe85125981b014e0d6afc979c3406dcd433
Kyle Mckay <kymckay.dev@gmail.com>
2022-02-14 21:32:09 +0000

    Add exercise 14 for lab 2

--- lab2/tasks/task14.js
+++ lab2/tasks/task14.js
@@ -1,3 +1,38 @@
-function task14(selector) {}
+function task14(selector) {
+  const bars = d3.select(selector)
+  .append('svg')
+    .attr('width', 60)
+    .attr('height', 100)
+  .selectAll('rect')
+    .data(d3.range(3))
+  .join('rect')
+    .attr('fill', 'blue')
+    .attr('x', d => d*20)
+    .attr('y', 0)
+    .attr('height', 20)
+    .attr('width', 10);
+
+  d3.select(selector)
+    .on('click', function() {
+      bars.each(function(d) {
+        d3.select(this)
+          .transition()
+            .ease(d3.easeLinear)
+            .duration(2000)
+            .delay(2000 * d)
+            .attr('height', 100)
+            .attr('fill', 'red')
+          .transition()
+            .ease(d3.easeLinear)
+            .duration(2000)
+            .attr('height', 10)
+            .attr('fill', 'blue');
+      });
+    });
+
+  d3.select(selector)
+    .append('p')
+      .text('On click, the bars will transition as before, but also in colour.');
+}
 
 export { task14 }
