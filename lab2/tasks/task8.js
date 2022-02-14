d9da44d680d3b1ccb5407d5573b706402f87fd6a
Kyle Mckay <kymckay.dev@gmail.com>
2022-02-14 17:53:31 +0000

    Add exercise 8 for lab 2

--- lab2/tasks/task8.js
+++ lab2/tasks/task8.js
@@ -1,3 +1,36 @@
-function task8(selector) {}
+function task8(selector) {
+  d3.select(selector)
+    .append('div')
+      .style('width',  '100px')
+      .style('height', '100px')
+      .style('background-color', 'blue')
+    .on('mouseenter', function() {
+      d3.select(this)
+        .interrupt()
+        .transition()
+          .duration(1000)
+          .style('background-color', 'red')
+          .style('width', '50px')
+          .style('height', '50px')
+        .transition()
+          .duration(2000)
+          .style('background-color', 'green')
+          .style('width', '200px')
+          .style('height', '200px');
+    })
+    .on('mouseleave', function() {
+      d3.select(this)
+        .interrupt()
+        .transition()
+          .duration(1000)
+          .style('background-color', 'blue')
+          .style('width', '100px')
+          .style('height', '100px');
+    });
+
+  d3.select(selector)
+    .append('p')
+      .text('On hover, the element will transition as before. It will return to default otherwise.')
+}
 
 export { task8 }
