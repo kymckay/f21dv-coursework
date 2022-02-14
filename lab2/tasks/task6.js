a3b781b511de7c57a799608aeda7cb36e1388538
Kyle Mckay <kymckay.dev@gmail.com>
2022-02-14 17:46:26 +0000

    Add exercise 6 for lab 2

--- lab2/tasks/task6.js
+++ lab2/tasks/task6.js
@@ -1,3 +1,26 @@
-function task6(selector) {}
+function task6(selector) {
+  d3.select(selector)
+    .append('div')
+      .style('width',  '100px')
+      .style('height', '100px')
+      .style('background-color', 'blue')
+    .on('click', function() {
+      d3.select(this)
+        .interrupt()
+        .transition()
+          .duration(1000)
+          .style('background-color', 'red')
+        .transition()
+          .duration(2000)
+          .style('background-color', 'green')
+        .transition()
+          .duration(1000)
+          .style('background-color', 'blue');
+    });
+
+  d3.select(selector)
+    .append('p')
+      .text('On click, the element will transition to red, then green and back to blue.')
+}
 
 export { task6 }
