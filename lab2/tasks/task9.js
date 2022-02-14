adf5344938fdc1ffcae7e74a75bac771d93c19be
Kyle Mckay <kymckay.dev@gmail.com>
2022-02-14 18:07:41 +0000

    Add exercise 9 for lab 2

--- lab2/tasks/task9.js
+++ lab2/tasks/task9.js
@@ -1,3 +1,36 @@
-function task9(selector) {}
+function task9(selector) {
+  const easeTypes = [d3.easeBounce, d3.easeSin, d3.easeCircle];
+
+  const divs = d3.select(selector)
+    .selectAll('div')
+      .data(easeTypes)
+    .join('div')
+      .style('width',  '100px')
+      .style('height', '100px')
+      .style('background-color', 'blue')
+      .style('transform', 'scale(1.0)');
+
+  d3.select(selector)
+    .on('click', function() {
+      divs.each(function(d) {
+        d3.select(this)
+          .interrupt()
+          .transition()
+            .ease(d)
+            .duration(1000)
+            .style('background-color', 'red')
+            .style('transform', 'scale(0.5)')
+          .transition()
+            .ease(d)
+            .duration(1000)
+            .style('background-color', 'blue')
+            .style('transform', 'scale(1.0)');
+      });
+    });
+
+  d3.select(selector)
+    .append('p')
+      .text('On click, the 3 elements will undergo the same transition with different easing functions.');
+}
 
 export { task9 }
