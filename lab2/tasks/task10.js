961bd71d9cca23b38f1963714ea9067f31f81d94
Kyle Mckay <kymckay.dev@gmail.com>
2022-02-14 18:14:40 +0000

    Add exercise 10 for lab 2

--- lab2/tasks/task10.js
+++ lab2/tasks/task10.js
@@ -1,3 +1,33 @@
-function task10(selector) {}
+function task10(selector) {
+  d3.select(selector)
+    .append('svg')
+      .attr('width', '200px')
+      .attr('height', '200px')
+    .append('circle')
+      .attr('cx', 100)
+      .attr('cy', 100)
+      .attr('r', 50)
+      .attr('fill', 'red')
+      .on('mouseenter', function() {
+        d3.select(this)
+          .interrupt()
+          .transition()
+            .duration(1000)
+            .ease(d3.easeBounce)
+            .attr('r', 100);
+      })
+      .on('mouseleave', function() {
+        d3.select(this)
+        .interrupt()
+        .transition()
+          .duration(1000)
+          .ease(d3.easeBounce)
+          .attr('r', 50);
+      });
+
+  d3.select(selector)
+  .append('p')
+    .text('The circle will change size when the mouse enters and exits, with a "bounce" easing.')
+}
 
 export { task10 }
