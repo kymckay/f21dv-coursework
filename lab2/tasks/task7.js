10e88dd3b9d35a382b86e5964a46a3170ac67fda
Kyle Mckay <kymckay.dev@gmail.com>
2022-02-14 17:47:06 +0000

    Add exercise 7 for lab 2

--- lab2/tasks/task7.js
+++ lab2/tasks/task7.js
@@ -1,3 +1,32 @@
-function task7(selector) {}
+function task7(selector) {
+  d3.select(selector)
+    .append('div')
+      .style('width',  '100px')
+      .style('height', '100px')
+      .style('background-color', 'blue')
+    .on('click', function() {
+      d3.select(this)
+        .interrupt()
+        .transition()
+          .duration(1000)
+          .style('background-color', 'red')
+          .style('width', '50px')
+          .style('height', '50px')
+        .transition()
+          .duration(2000)
+          .style('background-color', 'green')
+          .style('width', '200px')
+          .style('height', '200px')
+        .transition()
+          .duration(1000)
+          .style('background-color', 'blue')
+          .style('width', '100px')
+          .style('height', '100px');
+    });
+
+  d3.select(selector)
+    .append('p')
+      .text('On click, the element will transition colour as before, but also size.')
+}
 
 export { task7 }
