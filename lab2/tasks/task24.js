fc49c9e0d8017ec0996afdca02ddbd92e7719ff9
Kyle Mckay <kymckay.dev@gmail.com>
2022-02-22 21:30:11 +0000

    Add exercise 24 for lab 2

--- lab2/tasks/task24.js
+++ lab2/tasks/task24.js
@@ -1,3 +1,9 @@
-function task24(selector) {}
+function task24(selector) {
+  const intr = d3.interpolate([20, 40, 4], [1, 12, 10]);
+
+  d3.select(selector)
+    .append('p')
+      .text(`Type of returned function is: ${typeof(intr)}. Applied to 0.2 gives: ${intr(0.2)}.`);
+}
 
 export { task24 }
