bd01c44a9b94f07b4af3ace46881cf107169afbd
Kyle Mckay <kymckay.dev@gmail.com>
2022-02-22 21:38:08 +0000

    Add exercise 25 for lab 2

--- lab2/tasks/task25.js
+++ lab2/tasks/task25.js
@@ -1,3 +1,9 @@
-function task25(selector) {}
+function task25(selector) {
+  const cc = d3.interpolate('red', 'green')
+
+  d3.select(selector)
+    .append('p')
+      .text(`Interpolated colour value: ${cc(0.5)}.`);
+}
 
 export { task25 }
