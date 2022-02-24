1032d234e4932fe30794c1feb42c5126ff364354
Kyle Mckay <kymckay.dev@gmail.com>
2022-02-24 13:11:36 +0000

    Add exercise 26 for lab 2

--- lab2/tasks/task26.js
+++ lab2/tasks/task26.js
@@ -1,3 +1,11 @@
-function task26(selector) {}
+function task26(selector) {
+  const itp = d3.interpolateDate(
+    new Date(0), Date.now()
+  )
+
+  d3.select(selector)
+    .append('p')
+      .html(`Interpolated date half way from the Unix epoch (<code>1970-01-01T00:00:00Z</code>) until this moment: <code>${itp(0.5).toISOString()}</code>.`);
+}
 
 export { task26 }
