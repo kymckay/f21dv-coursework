4ebad57d608b8c2f069e36bac4355989560ef1c5
Kyle Mckay <kymckay.dev@gmail.com>
2022-03-01 19:29:17 +0000

    Add a basic slippy map with country interaction
    Using Leaflet to handle the geoJSON was easier than using D3

--- lab3/modules/map.js
+++ lab3/modules/map.js
@@ -1,12 +1,40 @@
-import { covidData } from './data.js';
-
-// const svg = d3.select('#map');
-
-// const projection = d3.geoMercator();
-
-// const pathGenerator = d3.geoPath()
-//   .projection(projection);
-
+/**
+ * Add a leaflet map to the page element with ID `map`.
+ * Loads world geoJSON and handles country highlighting.
+ */
 export async function makeMap() {
-  console.log(await covidData())
+  const map = L.map('map', {center: [20,0], zoom: 3});
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  const countries = await d3.json('world.json');
+
+  // Use Leaflet to automatically handle geoJSON with slippy map
+  L.geoJson(countries, {
+    style: () => ({ fillColor: 'red', opacity: 0, fillOpacity: 0}),
+    onEachFeature: function(_, layer) {
+      layer.on({
+        mouseover: onMouseOver,
+        mouseout: onMouseOut
+      })
+    }
+  }).addTo(map);
+
+  function onMouseOver(event) {
+    const { target } = event;
+
+    target.setStyle({
+      fillOpacity: 0.2,
+    })
+  }
+
+  function onMouseOut(event) {
+    const { target } = event;
+
+    target.setStyle({
+      fillOpacity: 0,
+    })
+  }
 }
