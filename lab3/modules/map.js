ff3d1644fc47e656a727c6323a1a286ea96f7136
Kyle Mckay <kymckay.dev@gmail.com>
2022-03-04 18:30:05 +0000

    Replace Leaflet based map with D3 powered one
    - Going to make interaction and updates easier to implement

--- lab3/modules/map.js
+++ lab3/modules/map.js
@@ -1,40 +1,50 @@
 /**
- * Add a leaflet map to the page element with ID `map`.
- * Loads world geoJSON and handles country highlighting.
+ * Loads world GeoJSON and adds a static (non-slippy) map to the page
+ * with interactivity.
  */
 export async function makeMap() {
-  const map = L.map('map', {center: [20,0], zoom: 3});
+  // Using viewbox for sizing so map can otherwise scale to screen
+  const svg = d3.select('#map')
+    .append('svg')
+      .attr('viewBox', '0 0 600 400');
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
+  // Mercator projection is familiar to most because so widly used
+  // Want to scale as large as convenient for easier interaction
+  // Translate to center the geometry in the viewbox
+  const projection = d3.geoMercator()
+    .scale(90)
+    .center([0,20])
+    .translate([300, 250]);
 
-  const countries = await d3.json('world.json');
+  const worldGeoData = await d3.json('world.json');
 
-  // Use Leaflet to automatically handle geoJSON with slippy map
-  L.geoJson(countries, {
-    style: () => ({ fillColor: 'red', opacity: 0, fillOpacity: 0}),
-    onEachFeature: function(_, layer) {
-      layer.on({
-        mouseover: onMouseOver,
-        mouseout: onMouseOut
-      })
-    }
-  }).addTo(map);
+  // TODO: Replace random colouring with something informative
+  const colorScale = d3.scaleSequential(d3.interpolateRainbow)
+    .domain(d3.extent(worldGeoData.features, d => d.properties.iso_n3));
 
-  function onMouseOver(event) {
-    const { target } = event;
+  // Construct global map from GeoJSON features, these contain ISO code
+  // for later joining with COVID-19 data (which I use as the ID here).
+  svg.append('g')
+    .selectAll('path')
+      .data(worldGeoData.features, d => d.properties.iso_a3)
+    .join('path')
+      .attr('d', d3.geoPath().projection(projection))
+      .attr('fill', d => colorScale(d.properties.iso_n3))
+      .on('mouseenter', onMouseEnter)
+      .on('mouseleave', onMouseLeave)
+      .on('click', onClick);
 
-    target.setStyle({
-      fillOpacity: 0.2,
-    })
+  function onMouseEnter() {
+    d3.select(this)
+      .attr('fill-opacity', 0.5);
   }
 
-  function onMouseOut(event) {
-    const { target } = event;
+  function onMouseLeave() {
+    d3.select(this)
+      .attr('fill-opacity', null);
+  }
 
-    target.setStyle({
-      fillOpacity: 0,
-    })
+  function onClick(_, data) {
+    console.log(data.properties);
   }
 }
