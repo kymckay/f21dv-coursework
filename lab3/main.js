f7baef2df1a031e9c0bb6c129b72c43dddf720a8
Kyle Mckay <kymckay.dev@gmail.com>
2022-03-10 10:27:09 +0000

    Add title identifying faceted data selection
    User needs this feedback to understand current state of the charts
    displayed

--- lab3/main.js
+++ lab3/main.js
@@ -3,6 +3,7 @@ import { covidData } from './modules/fetchers.js';
 import makeLineChart from './modules/line_chart.js';
 import { makeMap } from './modules/map.js';
 import { makeCountryLabel } from './modules/country_label.js';
+import { addModelListener } from './modules/model.js';
 
 // Load covid data on page load so that it's ready
 covidData();
@@ -26,3 +27,11 @@ makeLineChart(
   'people_fully_vaccinated',
   'cyan'
 );
+
+addModelListener('selectedCountry', async (iso_code) => {
+  if (!iso_code) return;
+
+  const data = await covidData();
+  d3.select('#charts_title')
+      .text(`Showing data for ${data[iso_code].location}`);
+});
