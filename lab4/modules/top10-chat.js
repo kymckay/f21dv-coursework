0058a7dbd19d49a63314ce16f2c4eefd3ac6f796
Kyle Mckay <kymckay.dev@gmail.com>
2022-04-07 16:45:27 +0100

    Allow changing years in the parallel chart
    A means of gaining insight into the changes in rank through all the
    years

--- lab4/modules/top10-chat.js
+++ lab4/modules/top10-chat.js
@@ -1,14 +1,34 @@
 import { ParallelChart } from './parallel-chart.js';
 import { getRankedNames } from './rank-chart.js';
 
+// Produce list of possible year transitions to show
+const years = d3.range(2020, 1996, -1);
+
 export class Top10Chart extends ParallelChart {
   constructor(id, data) {
     super(id);
 
-    const top10L = getRankedNames(data, '2019', 10);
-    const top10R = getRankedNames(data, '2020', 10);
+    this.dataset = data;
+    this.updateColumns(years[0].toString());
 
-    this.setLeft(Object.keys(top10L), '2019');
-    this.setRight(Object.keys(top10R), '2020');
+    this.controls
+      .append('select')
+      .classed('top10-chart-select', true)
+      .on('change', (event) => this.updateColumns(event.target.value))
+      .selectAll('option')
+      .data(years)
+      .join('option')
+      .attr('value', (d) => d)
+      .text((d) => `${d - 1}–${d}`);
+  }
+
+  updateColumns(year) {
+    const prevYear = (Number(year) - 1).toString();
+
+    const top10L = getRankedNames(this.dataset, prevYear, 10);
+    const top10R = getRankedNames(this.dataset, year, 10);
+
+    this.setLeft(Object.keys(top10L), prevYear);
+    this.setRight(Object.keys(top10R), year);
   }
 }
