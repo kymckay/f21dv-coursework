42b16965b3959ad092404819393f40c4767bff1e
Kyle Mckay <kymckay.dev@gmail.com>
2022-04-07 23:14:11 +0100

    Allow toggling initial count between per name/baby
    These two forms of count give different insights

--- lab4/modules/letter-chart.js
+++ lab4/modules/letter-chart.js
@@ -1,13 +1,17 @@
 import { BarChart } from './bar-chart.js';
 
-function getLetterFrequencies(data, year, initials = true) {
+function getLetterFrequencies(data, year, scaled = false, initials = true) {
   const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
   const yearData = d3.group(data, (d) => d.year).get(new Date(year));
 
   return letters.reduce((pv, cv) => {
     const reducer = initials
-      ? (pvd, cvd) => pvd + Number(cvd.name.charAt(0) === cv)
-      : (pvd, cvd) => pvd + cvd.name.split('').filter((c) => c === cv).length;
+      ? (pvd, cvd) =>
+          pvd + Number(cvd.name.charAt(0) === cv) * (scaled ? cvd.count : 1)
+      : (pvd, cvd) =>
+          pvd +
+          cvd.name.split('').filter((c) => c === cv).length *
+            (scaled ? cvd.count : 1);
 
     pv[cv] = yearData.reduce(reducer, 0);
 
@@ -33,11 +37,25 @@ export class LetterChart extends BarChart {
       .attr('value', (d) => d)
       .text((d) => d);
 
+    const checkDiv = this.controls.append('div');
+
+    checkDiv.append('label').text('Count per-baby:');
+
+    checkDiv
+      .append('input')
+      .attr('type', 'checkbox')
+      .attr('title', 'Perform weighted sum by number of each baby given a name')
+      .on('input', () => {
+        this.checked = !this.checked;
+        this.updateYear(this.year);
+      });
+
     this.updateYear('2020');
   }
 
   updateYear(year) {
-    const freqs = getLetterFrequencies(this.dataset, year);
+    this.year = year;
+    const freqs = getLetterFrequencies(this.dataset, year, this.checked);
 
     // Sort the bins by value to make rank comparison easy for end user
     this.updateAxes(
